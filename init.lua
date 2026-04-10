4ec8b9b0c71837b079ee65361ea1e57ead760190
Joshua Blokland (External) <joshua.blokland_external@wartsila.com>
2026-04-10 09:56:23 +0200

    feat: add key binding for copy current file path to clipboard

--- init.lua
+++ init.lua
@@ -7,4 +7,8 @@ vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.diagnostic.config({ virtual_text = true })
 
+vim.keymap.set('n', '<leader>cp', function()
+  vim.fn.setreg('+', vim.fn.expand('%:p'))
+end, { desc = 'Copy file path to clipboard' })
+
 require("config.lazy")
