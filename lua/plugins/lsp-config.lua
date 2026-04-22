5d5fed694da85a111b7cc5dd6405608e46d90b58
Joshua Blokland (External) <joshua.blokland_external@wartsila.com>
2026-04-22 12:54:31 +0200

    feat: add key binding to display lsp errors

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -42,6 +42,7 @@ return {
           vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
           vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+          vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
         end
       })
     end
