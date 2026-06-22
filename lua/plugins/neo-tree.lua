64044b6ed4f6c47b083fe59ac5a9b83e801e3965
Joshua Blokland <joshua.blokland@wartsila.com>
2026-06-22 14:59:17 +0200

    feat: show hidden files and folders in the neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,16 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_gitignored = false,
+          hife_dotfiles = false
+        }
+      }
+    },
+    lazy = false,
     config = function()
       vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neotree' })
     end
